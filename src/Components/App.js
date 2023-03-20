8fbb236f3d824434b8103194986f4b2142e60fb0
stephen ligtenberg <bountyhunter24@gmail.com>
2023-03-20 11:16:47 -0400

    initial data fetching on page load

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,16 +1,35 @@
-import React from "react";
+import React, {useState, useEffect } from "react";
 import Header from "./Header"
 import Filter from "./Filter";
 import NewTask from "./NewTask";
 import NewCategory from "./NewCategory";
 
 function App() {
+  const [categories, setCategories] = useState([])
+  const [tasks, setTasks] = useState([])
+
+  // console.log(tasks)
+  // console.log(categories)
+
+  // execute on load
+  useEffect(() => {
+    // get categories from server
+    fetch("http://localhost:9292/categories")
+      .then(r => r.json())
+      .then(setCategories)
+    
+    // get tasks from server
+    fetch("http://localhost:9292/tasks")
+      .then(r => r.json())
+      .then(setTasks)
+  }, [])
+
   return (
     <div >
       <Header />
-      <Filter />
-      <NewTask />
-      <NewCategory />
+      <Filter categories={categories} tasks={tasks}/>
+      <NewTask setTasks={setTasks}/>
+      <NewCategory setCategories={setCategories}/>
     </div>
   );
 }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,9 +2,9 @@ import React from "react";
 
 function Header() {
     return (
-        <div>
-            * Header *
-        </div>
+        <header>
+            Stephen's todo list
+        </header>
     )
 }
 
