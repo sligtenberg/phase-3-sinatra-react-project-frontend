827fefcacacf22a077b8250738bbb9e254d809ec
stephen ligtenberg <bountyhunter24@gmail.com>
2023-03-21 18:25:29 -0400

    statefull new category color

--- src/Components/NewCategory.js
+++ src/Components/NewCategory.js
@@ -2,16 +2,17 @@ import React, {useState } from "react";
 
 function NewCategory() {
     const [newCategoryMode, setNewCategoryMode] = useState(false)
+    const [color, setColor] = useState("#D3D3D3")
 
     return (
-        <div className="category" style={{backgroundColor: "lightgrey"}}>
+        <div className="category" style={{backgroundColor: color}}>
             <h3>New Category</h3>
             <button onClick={() => setNewCategoryMode(!newCategoryMode)}>{newCategoryMode ? "Cancel" : "New Category"}</button>
             {newCategoryMode ?
                 <form>
                     <input type="text" placeholder="Name"></input>
-                    <label forHTML="color">Color:</label>
-                    <input id="color" type="color"></input>
+                    <label htmlFor="color">Color:</label>
+                    <input id="color" type="color" value={color} onChange={event => setColor(event.target.value)}></input>
                     <input type="submit"></input>
                 </form> :
                 null}
